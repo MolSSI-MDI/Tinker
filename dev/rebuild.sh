f1db58aa40e56f5cbf14da52cb58b8b45251d955
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-01-13 14:57:38 -0500

    Update rebuild.sh

--- dev/rebuild.sh
+++ dev/rebuild.sh
@@ -1,5 +1,10 @@
-cd ../build/source
+# Tinker Makefiles expect everything to be in $(HOME)/tinker
+cd ../build
+HOME=$(pwd)
+
+
+cd tinker/source
 
 # Compile Tinker
-cp ../../source/* .
+cp ../../../source/* .
 make -j 4
